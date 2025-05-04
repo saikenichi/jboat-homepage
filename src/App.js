986519fe838d5f17d52a19bdaddb3681b674adcf
Choi Hyunil <saikenichi12@gmail.com>
2025-05-04 15:59:31 +0900

    처음 업로드

---
+++ src/App.js
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <img
+        src="https://images.unsplash.com/photo-1549924231-f129b911e442"
+        alt="요트"
+        style={{ width: '100%', maxHeight: '400px', objectFit: 'cover' }}
+      />
+      <h1>JBOAT 홈페이지</h1>
+      <p>일본 중고 보트를 소개하고 구매를 도와드립니다.</p>
+      <button style={{ padding: '10px 20px', fontSize: '16px', marginTop: '20px' }}>
+        구매 의뢰하기
+      </button>
+    </div>
+  );
+}
+
+export default App;
